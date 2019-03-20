67cce0a1e233b9752e0d1cb66c263174d1d7e096
Sonali Hiranandani <hiranandanisonal@gmail.com>
2019-03-20 16:34:42 +0530

    initial prototype

---
+++ Assets/Menuuu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+// using System.Diagnostics;
+using System;
+
+public class Menuuu : MonoBehaviour
+{
+   public void ChangeScene(string sceneName)
+   {
+       Application.LoadLevel(sceneName);
+   }
+}
